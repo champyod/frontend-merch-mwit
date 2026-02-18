be6954632edfc1bf695564e79c0cdc02cffb5ed1
CCYod <champyodsakon@gmail.com>
2026-02-18 10:13:09 +0700

    initial: fresh frontend from template

---
+++ components/ui/SocialReminder.tsx
@@ -0,0 +1,30 @@
+import { BRAND_SOCIALS } from "@/app/config";
+
+export default function SocialReminder() {
+	return (
+		<div className="border-4 border-dotted border-red-500 p-3 max-w-sm bg-white">
+			<p className="pt-3">
+				<strong>NOTICE:</strong> The website only introduces products,
+				customers please contact{" "}
+				<a
+					href={BRAND_SOCIALS.facebook}
+					target="_blank"
+					rel="noreferrer"
+					className="text-blue-500 underline underline-offset-4 font-bold"
+				>
+					Facebook
+				</a>{" "}
+				or{" "}
+				<a
+					href={BRAND_SOCIALS.instagram}
+					target="_blank"
+					rel="noreferrer"
+					className="text-blue-500 underline underline-offset-4 font-bold"
+				>
+					Instagram
+				</a>{" "}
+				to make payment.
+			</p>
+		</div>
+	);
+}
